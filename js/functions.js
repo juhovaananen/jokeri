1a8239a26296d64359fcc94f0545b1675b819f14
juhovaananen <n3vaju00@students.oamk.fi>
2024-02-19 19:12:30 +0200

    Projekti valmis

---
+++ js/functions.js
@@ -0,0 +1,26 @@
+var arvotut = 0
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    let luoNappi = document.querySelector('button');
+    let taulukko = document.querySelector('table');
+
+    luoNappi.addEventListener('click', generateJokerRow);
+    
+
+
+    function generateJokerRow() {
+        let uusiRivi = document.createElement('tr');
+        for (let i = 0; i < 7; i++) {
+            let randoomiNum = Math.floor(Math.random() * 10);
+            let Celli = document.createElement('td');
+            Celli.textContent = randoomiNum;
+            uusiRivi.appendChild(Celli);
+        }
+        taulukko.appendChild(uusiRivi);
+        arvotut++
+        document.querySelector("p").innerHTML = "Arvotut rivit " + arvotut
+    }
+    
+});
